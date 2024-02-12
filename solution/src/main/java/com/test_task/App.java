edb72746aa0718cca4a53d1e48f8ea5b602f9296
Super Thaddeus <supertha@student.21-school.ru>
2024-02-12 18:21:31 +0700

    Initial commit

---
+++ solution/src/main/java/com/test_task/App.java
@@ -0,0 +1,13 @@
+package com.test_task;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
